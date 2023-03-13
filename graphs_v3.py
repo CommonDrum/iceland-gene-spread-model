b0f9370a734859c1cfeda96cebd58d2e0ad2fb78
commondrum <karoljhetman@gmail.com>
2023-03-13 19:49:51 +0000

    improved graphs_v3

--- graphs_v3.py
+++ graphs_v3.py
@@ -42,19 +42,35 @@ def init_graph(G,pairs, singles):
 
     for i in range(singles):
         G.add_node(uuid.uuid1().int, age=random.randint(0,2), sex=random.choices(sex), is_infected=random.choices(infection, weights_infection)[0], partner=None,family = [])
-               
+    
+    #for each node find 5 random nodes and create an edge between them.
+    # Without duplicates and self loops
+    for i in G.nodes:
+        for j in random.sample(list(G.nodes),5):
+            if i!=j:
+               G.add_edge(i,j)
 
 
 
+#change color of nodes based on sex and infection
+def color_nodes(G):
+    colors = []
+    for i in G.nodes:
+        if G.nodes[i]["sex"] == 'M':
+            colors.append("green")
+        elif G.nodes[i]["is_infected"] == True:
+            colors.append("red")
+        else:
+            colors.append("blue")
+    return colors
 
 
-# use "family" variable this will ease up the process of pairing etc. 
-# this can iterate through three generations of a family befor pairing
+init_graph (G,100,100)
+color = color_nodes(G)
+#nx.draw(G, node_color=color, with_labels=True)
 
-
-init_graph (G,10,10)
 # draw the graph
-nx.draw(G)
+nx.draw(G,node_color=color)
 
 # show the graph
 plt.show()
