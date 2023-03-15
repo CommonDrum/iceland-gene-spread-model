d0b9d63aa016e85c9efb5aa84f1fc0777e532092
commondrum <karoljhetman@gmail.com>
2023-03-15 14:46:32 +0000

    Version 4 works

--- graphs_v4.py
+++ graphs_v4.py
@@ -24,7 +24,10 @@ class GraphInterface():
         self.weights_children = [0.3, 0.2, 0.4, 0.1]
 
         self.sex = ['F','M']
-        self.reproduction_rate = 0.9
+        self.reproduction_rate = 0.23
+
+        self.population = 0
+        self.infected = 0
         
 
 
@@ -35,6 +38,9 @@ class GraphInterface():
         for i in family:
             if i != id:
                 self.G.add_edge(id,i, family = True)
+        self.population += 1
+        if is_infected:
+            self.infected += 1
         return id
     
     def find_friends(self,new_friends = 2):
@@ -51,7 +57,6 @@ class GraphInterface():
     
     def pair(self):
         for u, v, attrs in self.G.edges(data=True):
-            print(u,v,attrs)
             if attrs.get('family') == False:
                 # If the nodes are not already a couple
                 if self.G.nodes[u]['partner'] == 0 and self.G.nodes[v]['partner'] == 0:
@@ -63,14 +68,18 @@ class GraphInterface():
             #if node is female (to prevent duplicate children) and is has a partner
             if self.G.nodes[i]['partner'] != 0 and self.G.nodes[i]['sex'] == 'F' and random.random() < self.reproduction_rate:
                 #in the future inherite the infection status from the parents
+                if self.G.nodes[i]['is_infected'] and self.G.nodes[self.G.nodes[i]['partner']]['is_infected']:
+                    is_infected = True
+                else:
+                    is_infected = False 
+
                 family_nodes = [i,self.G.nodes[i]['partner']]
                 for neighbor in self.G.neighbors(i):
                     if self.G.get_edge_data(i, neighbor).get('label') == 'family':
                         family_nodes.append(neighbor)
 
-                child = self.new_node(family=family_nodes)
+                child = self.new_node(family=family_nodes, is_infected=is_infected)
                 #add all family nodes from both partners
-                print ("child")
 
     def ageing(self):
         for i in list(self.G.nodes):
@@ -80,21 +89,34 @@ class GraphInterface():
                 #change the partner status of the partner
                     p = self.G.nodes[i]['partner']
                     self.G.nodes[p]['partner'] = 0
+                self.population -= 1
+                if self.G.nodes[i]['is_infected']:
+                    self.infected -= 1
                 self.G.remove_node(i)
-                print ("dead")
+    def initialize(self,size):
+        for i in range(size):
+            self.new_node(is_infected=random.choices(self.infection, self.weights_infection)[0])
+    
+
 
 
 
     
 G = GraphInterface()
-G.new_node(sex="F")
-G.new_node(sex="M")
-
-for i in range (10):
+G.initialize(1000)
+populaion_list = []
+infected_list = []
+for i in range (100):
     G.find_friends()
     G.pair()
     G.reproduce()
     G.ageing()
+    populaion_list.append(G.population)
+    infected_list.append(G.infected)
+
+plt.plot(populaion_list)
+plt.plot(infected_list)
+plt.show()
 
 """
 # Get a random edge and read its attributes
